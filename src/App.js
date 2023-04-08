aa07240ab7320bc12c63730d72037de7f09ae906
BaoBui <buitragiabao2016@gmail.com>
2023-04-08 22:28:55 +0700

    basic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
 function App() {
+  const x = 5;
+  const y = 7;
+  const z = x + y;
+  console.log(x);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>ReactJs</h1>
+      <div>Hôm nay tôi học bài</div>
+      <div>{z}</div>
+    </>
   );
 }
 
